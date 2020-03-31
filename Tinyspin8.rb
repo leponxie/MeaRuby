298c76b00a64820cde7f566b7160d2db3a79fbd4
leponxie <58828099+leponxie@users.noreply.github.com>
2020-03-31 17:58:57 +0300

    Add files via upload
    Tiny little spinner in Ruby.

---
+++ Tinyspin8.rb
@@ -0,0 +1,33 @@
+x = 0.1
+
+# while true
+
+11.times do
+
+print "/" + "\r"
+sleep x
+            
+print "-" + "\r"
+sleep x
+
+print "\\" + "\r"
+sleep x
+
+print "|" + "\r"
+sleep x
+
+print "/" + "\r"
+sleep x
+
+print "-" + "\r"
+sleep x
+
+print "\\" + "\r"
+sleep x
+
+print "|" + "\r"
+sleep x
+
+end
+
+
